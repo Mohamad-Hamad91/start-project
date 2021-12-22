7646928b65815852a5d4f8a0e70c6158af6e63a9
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2021-12-22 16:09:10 +0200

    set environment variables in package.json

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ErrorsInterceptor } from './utils/logger/errors.interceptor';
 import { MyLogger } from './utils/logger/my-logger';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
 
@@ -10,6 +12,14 @@ async function bootstrap() {
     logger: new MyLogger(),
     // logger: false
   });
+
+  app.use(helmet());
+  app.use(csurf());
+
+
+  if (process.env.NODE_ENV !== 'production')
+    app.enableCors();
+
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   const myLogger = app.get(MyLogger);
